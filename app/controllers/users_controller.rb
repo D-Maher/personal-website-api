81a0156675f4417b73b09308df7e512fec20ea00
D-Maher <dmaher724@gmail.com>
2018-02-22 18:06:10 -0600

    Flesh out users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,54 @@
 class UsersController < ApplicationController
+  # GET /users
+  def index
+    render json: User.all
+  end
+
+
+  # GET /users/:user_id
+  def show
+    render json: @user
+  end
+
+  # POST /users
+  def create
+    user = User.create(user_params)
+
+    if user.save
+      render json: user
+    else
+      render user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /users/:user_id
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/:user_id
+  def destroy
+    @user.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+      params.require(:user).permit(
+        :email,
+        :password,
+        :password_confirmation,
+        :f_name,
+        :l_name
+      )
+    end
 end
